27df3421bd1203b930455c4338bfef7f4b32ed71
xop141 <lowsens400@gmail.com>
2024-12-02 20:14:29 -0800

    java spoj
    First page

---
+++ js.js
@@ -0,0 +1,344 @@
+
+
+// let a = 42213;
+// let b = a % 100;
+// let x = a % 10;
+// let c = (b - x) / 10
+
+
+// let a = 27
+// let b = a % 10
+// let c = (a - b) / 10
+// let d = b * c
+
+
+// let a = 123
+// let c = (a % 100) % 10
+// let d = (a-c)%100 / 10
+// let e = (a - c - (d*10))/100
+// let f = c + d + e
+
+
+
+// let a = 200
+// let b = a % 60
+// let c = a - b
+// let d = c / 60
+
+// let a = 3612
+// let s = a % 3600
+// let d = (a - s) / 3600
+// console.log(s, d , m);
+
+
+
+
+// let a = 2
+// let b = 4
+// console.log(a*60 + b);
+
+// let a = 84
+// let b = Math.floor(a / 24)
+// let c = a - 24 * b
+// console.log(b, c);
+
+
+// let a = 1
+// let b = 5
+// console.log(b+ a * 24);
+
+
+// let a = 25
+// let b = Math.floor(33/12)
+// console.log(b, a-b*12);
+
+// let a = 2
+// let b = 3
+// console.log(a*12 + b);
+
+
+// let a = 2
+// let b = 22
+// if (a>b){
+    // console.log(a);
+// } else if (a<b){
+    // console.log(b);
+// } else{
+    // console.log("=");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//let a = 81, b = 80, c = 80, d = 80, x = 0
+//if (a>80){
+//    x = x + a
+//}
+//if (b>80){
+//    x = x + b
+//}
+//if (c>80){
+//    x = x + c
+//}
+//if (d>80){
+//    x = x + d
+//}
+//console.log(x);
+
+//let a = 1, b = 4, c = 7, d = 8, x = 1
+//if (a>5){
+//    x = x * a
+//}
+//if (b>5){
+//    x = x * b
+//}
+//if (c>5){
+//    x = x * c
+//}
+//if (d>5){
+//    x = d * x
+//}
+//console.log(x);
+
+//let a = 1, b = 2, c = 7, x = 0
+//if ((a%2) > 0) {
+//    x = x
+//}else{
+//    x = x + a
+//}
+//if ((b%2) > 0) {
+//    x = x
+//}else{
+//    x = x + b 
+//}
+//if ((c%2) > 0) {
+//    x = x
+//}else{
+//    x = x + c
+//}
+//console.log(x);
+
+//let a = 5, b = 0, c = 10
+//if (a = 5) {
+//    console.log(a);
+//} else{
+//    console.log("");
+//}
+//if (b = 5) {
+//    console.log(b);
+//}
+//if (c = 5) {
+//    console.log(c);
+//}
+
+//let a = 15, b = 15, c = 3, d = 12
+//if ((a % 3) == 0) {
+//    console.log(a);
+//}
+//if ((b % 3) == 0) {
+//    console.log(b);
+//}
+//if ((c % 3) == 0) {
+//    console.log(c);
+//}
+//if ((d % 3) == 0) {
+//    console.log(d);
+//}
+
+
+//let a = 11, b = 22, c = 11, d = 15, x = 0
+//if (a%11 > 0){
+//    x = x + a
+//}
+//if (b%11 > 0){
+//    x = x + b
+//}
+//if (c%11 > 0){
+//    x = x + c
+//}
+//if (d%11 > 0){
+//    x = x + d
+//}
+//console.log(x);
+
+//let a = 10
+//if(a > 10){
+//console.log("yes");
+//} else{
+//console.log("no");
+//}
+
+//let a = 10
+//if (a<5) {
+//    console.log("yes");
+//} else{
+//    console.log("no");    
+//}
+
+// console.log("IOI");
+
+// let b = 0
+// while (b<3) {
+//  b = b+1
+//  console.log("IOI");
+// }
+
+// let a = 10;
+// while (a > 0){
+// console.log("IOI");
+// a = a - 1;
+// }
+
+// let a = 5
+// console.log((1+a)/2*a);
+
+// let a = 5
+// const c = a
+// b = 1
+// while (a > 1){
+    // a = a - 1
+    // b = b * a
+// }
+// console.log(b*c);
+
+// let n = 5
+// let a = 0
+// while (n >= 1) {
+    // a = a + 100
+    // n = n - 1
+// }
+// console.log(a);
+
+
+
+// let a = 30
+// let b = 5
+// console.log(a, b);
+// console.log(a*b);
+
+// let a = 3
+// let b = 1
+// while (a>=1){
+    // a = a - 1
+    // b = 2 * b 
+// }
+// console.log(b);
+
+// let a = 3
+// let b = 3
+// const c = b
+// while (a > 1) {
+    // b = b * c
+    // a = a - 1
+// }
+   // console.log(b);
+
+// let a = 1
+// if (a>2) {
+    // console.log("tentssen");
+// } else {
+    // console.log("gg");
+// }
+
+// let a = 2
+// if (a == 2) {
+    // console.log("muu")
+// }
+// if (a == 3) {
+    // console.log("dund")
+// }
+// if (a == 4){
+    // console.log("sain")} 
+// if (a == 5) {
+    // console.log("onts");
+// }    
+
+
+// let a = 69
+// if (a<60) {
+    // console.log("gg");
+// } else if (a<70){
+    // console.log("d");
+// } else if (a<80){
+    // console.log("c");
+// } else if(a<90){
+    // console.log("b");
+// } else {
+    // console.log("a");
+// }
+
+
+// let a = 2, b = 2, c = 5, x=0
+//  if (a+b>c){
+//  if (a+c>b) {
+    //  x = x+1
+    //  }
+// }
+//  if (b+c>a) {
+    //  if (b+a>c) {
+    // x=x+1
+    //  }    
+//  }
+//  if (x==2) {
+    // console.log("g");
+// }
+
+
+// let a = 3
+// let c = 11
+// while (c > 1) {
+    // c = c - 1
+    // console.log(c*a);
+// }
+
+
+
+//  let a = 5, b = 2, d = 1;
+//  const c = b;
+//  while (a > 0) {
+    // console.log(`${c}${"^"}${d}${"="}${b}`);
+    // b = b * c;
+    // a = a - 1;
+    // d = d + 1;
+//  }
+
+//  let a = 2, b = 3, c = 4
+//  console.log(a*b-c);
+ 
+//  let davhar = 5, haalga = 4, toot = 10
+//  console.log(`${Math.ceil(toot/haalga)} ${"davhar"}`);
+//  console.log(toot%haalga);
+ 
+ let orts = 3
+ let haalga = 5
+ let davhar = 4
+ let toot = 50
+ console.log(Math.ceil(toot/haalga/davhar), "orts");
+ console.log(toot - Math.ceil(toot/haalga/davhar)*(davhar * haalga));
+ 
+
+ 
+
+ 
+ 
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
